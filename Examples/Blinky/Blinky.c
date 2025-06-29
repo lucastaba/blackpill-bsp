653a3f21cd1d79d20e6eadca8503822732268a3c
Lucas Taba <lucas.taba3@gmail.com>
2025-06-29 01:43:54 -0300

    working blinking example and realease 1.0.0

--- Examples/Blinky/Blinky.c
+++ Examples/Blinky/Blinky.c
@@ -5,30 +5,25 @@
 
 static void delay(uint32_t ms);
 
-void main_app(void)
-{
+void main_app(void) {
     vioInit();
-    while (1)
-    {
+    while (1) {
         vioSetSignal(vioLED3, vioLEDon);
-        delay(500);
+        delay(500U);
         vioSetSignal(vioLED3, vioLEDoff);
-        delay(500);
+        delay(500U);
     }
 }
 
-static void delay(uint32_t ms)
-{
+static void delay(uint32_t ms) {
     uint32_t delayTime;
 
-    if (ms > UINT32_MAX/100000)
-    {
+    if (ms > ((UINT32_MAX / 100000U) * 4U)) {
         return;
     }
 
-    delayTime = ms*100000;
-    while (delayTime--)
-    {
+    delayTime = ms * 100000U / 4U;
+    while (delayTime--) {
         __NOP();
     }
 }
--- Examples/Blinky/STM32CubeMX/BlackPill/MX_Device/MX_Device.h
+++ Examples/Blinky/STM32CubeMX/BlackPill/MX_Device/MX_Device.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File Name   : MX_Device.h
- * Date        : 28/06/2025 11:39:47
+ * Date        : 28/06/2025 23:00:19
  * Description : STM32Cube MX parameter definitions
  * Note        : This file is generated with a generator out of the
  *               STM32CubeMX project and its generated files (DO NOT EDIT!)
